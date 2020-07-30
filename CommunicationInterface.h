cc4e9f0a32afe1e51cac4e310d1f8b8223a7dfd3
tarcaflorica <tarcaflorica@yahoo.com>
2020-07-30 09:54:09 +0300

    tralala

--- CommunicationInterface.h
+++ CommunicationInterface.h
@@ -1,3 +1,5 @@
+#include "CI_Defines.h"
+
 bool CI_getUndervoltage();
 void CI_setUnderVoltage(bool undervoltageForSet_b);
 bool CI_getOverVoltage();
@@ -11,16 +13,16 @@ void CI_setLCW_Warning(Warning_Feature *LCW_Warning_ForSet_st);
 void CI_Read_data();
 void CI_Write_data(int option_i, int data_i);
 unsigned char CI_getSpeed();
-void CI_setSpeed(unsigned char speedForSet_uc)
+void CI_setSpeed(unsigned char speedForSet_uc);
 Gear CI_getGear();
 void CI_setGear(Gear gearForSet_e);
 unsigned char CI_getAngle();
-void CI_setAngle(unsigned char angleForSet_uc)
+void CI_setAngle(unsigned char angleForSet_uc);
 unsigned char CI_getDistance();
-void CI_setDistance(unsigned char distanceForSet_uc)
+void CI_setDistance(unsigned char distanceForSet_uc);
 unsigned char CI_getBatteryVoltage();
 void CI_setBatteryVoltage(unsigned char battery_voltageForSet_uc);
 Ssm CI_getCurrent_ssm_state();
 void CI_setCurrent_ssm_state(Ssm current_ssm_stateForSet_e);
-void CI_getGlobalErr_st(ErrList *globalErrForGet_st)
-void CI_setGlobalErr_st(ErrList *globalErrForSet_st)
+void CI_getGlobalErr_st(ErrList *globalErrForGet_st);
+void CI_setGlobalErr_st(ErrList *globalErrForSet_st);
