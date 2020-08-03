2f90a0ea5b3273d0d1cb9ae4950f542b60908da2
MatesClaudia <claudia.mates98@gmail.com>
2020-08-03 11:29:54 +0300

    requirements added

--- scheduler.c
+++ scheduler.c
@@ -26,9 +26,6 @@ int main(){
     clock_t before = clock();
     ErrStatus MYerr_e[2];
     ErrStatus myBatErr_e, myLostComErr_e;
-    // MYerr_st[0].errQualTime_c=2; MYerr_st[0].errDequalTime_c =5; MYerr_st[0].errName_e=err_lostcom; MYerr_st[0].errStatus_e=passed;
-    // MYerr_st[1].errQualTime_c=1; MYerr_st[1].errDequalTime_c =4; MYerr_st[1].errName_e=err_batvoltage; MYerr_st[1].errStatus_e=passed; 
-    // MYerr_st[2].errQualTime_c=3; MYerr_st[2].errDequalTime_c =7; MYerr_st[2].errName_e=err_lostcom; MYerr_st[2].errStatus_e=passed; 
     int menu_decision_uc;
     int y=0;
     bool under_b;
@@ -55,9 +52,7 @@ int main(){
                         printf("Iteratia curenta e: %d\n",x);
                         errorHandler_BatteryVoltage_Check();
                         errorHandler_Communication_Check();
-                        //CI_getGlobalErr_st(MYerr_st);
                         errorHandler_GetError(err_batvoltage,&myBatErr_e);
-                        //Ssm crt_smm_state1 = systemStateMachine_change_state(MYerr_st,&myTrigger_i);
                         errorHandler_GetError(err_lostcom,&myLostComErr_e);
                         printf("Battery Voltage Error: %s and Lost Communication Error: %s\n",CI_getErrStatusChar(myBatErr_e),CI_getErrStatusChar(myLostComErr_e));
                         MYerr_e[0] = myBatErr_e, MYerr_e[1]=myLostComErr_e;
